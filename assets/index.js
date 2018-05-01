54d80843837553d2813e373eb0401d1f0687fa77
Mohammed <mido5219@gmail.com>
2018-05-02 00:49:46 +0200

    adding real time animation to process handling

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,8 @@
-let processes = [], WaitingProcesses = [],clock = 0,totalbt =0,processNumber=0
+let processes = [],
+  WaitingProcesses = [],
+  clock = 0,
+  totalbt = 0,
+  processNumber = 0
 let burstTimeField = document.querySelector('#burst_time'),
   arrivedTimeField = document.querySelector('#arrived_time'),
   addbtn = document.querySelector('#addbtn')
@@ -32,7 +36,7 @@ AddProcess = () => {
   const burstTime = burstTimeField.value,
     arrivedTime = arrivedTimeField.value
   if (!(burstTime.at < 0 || arrivedTime < 0)) {
-    totalbt+=parseInt(burstTime)
+    totalbt += parseInt(burstTime)
     current = {
       name: `p${++processNumber}`,
       bt: parseInt(burstTime),
@@ -43,7 +47,7 @@ AddProcess = () => {
     processes.push(current)
   }
 
- 
+
   let row = document.createElement('tr'),
     processName = document.createElement('td'),
     processBurstTime = document.createElement('td'),
@@ -89,64 +93,86 @@ StartSimulation = () => {
   addbtn = document.querySelector('#addbtn').disabled = true
 
   let processCounter = 0
-  let dp = processes[0], running = processes[0]
-  let prepare = ()=>{
-    processes.forEach((process)=>{
-      if(process.at < running.at)
-      dp = process
+  let dp = processes[0],
+    running = processes[0]
+  let prepare = () => {
+    processes.forEach((process) => {
+      if (process.at < running.at)
+        dp = process
       running = dp
     })
     WaitingProcesses.push(running)
     processes.splice(processes.findIndex(p => p.name == running.name), 1)
   }
-    let ProcessScan =  setInterval (()=>{
-  
-       processes.forEach((process)=>{
-          if(process.at == clock){
-            WaitingProcesses.push(process)
+  let ProcessScan = setInterval(() => {
 
+    processes.forEach((process) => {
+      if (process.at == clock) {
+        WaitingProcesses.push(process)
+        let processBox = document.createElement('div')
+
+        processBox.classList.add('col')
+        let unit = (100 - (processes.length)) / totalbt
+        console.log(unit)
+        let width = process.bt * unit
+        processBox.setAttribute("style", "width:" + width + "%")
+        processBox.id = process.name
+        processBox.classList.add('z-depth-3')
+        processBox.classList.add('processBox')
+        processBox.style.margin = "0.5%"
+        processBox.style.background = RandomColor()
+        processBox.style.transform = `translateX(100%)`
+        processBox.style.opacity = `0`
+
+        let processNameBox = document.createElement('h5')
+        processNameBox.textContent = process.name
+        processBox.appendChild(processNameBox)
+        timeline.appendChild(processBox)
+        let x = 200
+        let y = 0
+        let scaler = setInterval(() => {
+          processBox.style.transform = `translateX(${--x}%)`
+          if (y < 1) {
+            processBox.style.opacity = `${y+=0.004}`
           }
-        })
-  
-  
-        if(running.progress == running.bt){
-          running.done = true
-          processCounter ++
-          dp = WaitingProcesses.shift()
-          WaitingProcesses.sort((p1,p2) => p1.bt-p2.bt)
-          running = WaitingProcesses[0]
-          let processBox = document.createElement('div')
-          
-          processBox.classList.add('col')
-          let unit = (100-(processes.length))/totalbt
-          console.log(unit)
-          let width = dp.bt*unit
-          processBox.setAttribute("style", "width:" + width + "%")
-          processBox.classList.add('z-depth-3')
-         processBox.style.margin="0.5%"
-          processBox.style.background = RandomColor()
-          processBox.style.transform = `translateX(100%)`
-          processBox.style.opacity = `0`
-          
-          let processNameBox = document.createElement('h5')   
-          processNameBox.textContent = dp.name
-          processBox.appendChild(processNameBox)
-            timeline.appendChild(processBox)
-            let x = 200
-            let y = 0
-          let scaler = setInterval(() => {
-            processBox.style.transform = `translateX(${--x}%)`
-            if(y<1){
-              processBox.style.opacity = `${y+=0.004}`
-            }
-            x ==0 ? clearInterval(scaler) : ""
-          }, 1)
-        }else{
-          running.progress++
+          x == 0 ? clearInterval(scaler) : ""
+        }, 1)
       }
-        clock++
-        WaitingProcesses.length>0?'':clearInterval(ProcessScan)
-      },1000)
+    })
+
+
+    if (running.progress == running.bt) {
+      running.done = true
+      processCounter++
+      dp = WaitingProcesses.shift()
+      WaitingProcesses.sort((p1, p2) => p1.bt - p2.bt)
+      running = WaitingProcesses[0]
+
+      let processesBoxes = document.querySelectorAll('.processBox')
+      let DoneBox
+      processesBoxes.forEach((process) => {
+        if (dp.name == process.id) {
+          DoneBox = process
+        }
+      })
+      let x = 0
+          let y = 1
+          let scaler = setInterval(() => {
+            DoneBox.style.transform = `translateY(${++x}%)`
+            if (y > 0) {
+              DoneBox.style.opacity = `${y-=0.004}`
+            }
+            x == 200 && y==0 ? clearInterval(scaler) : ""
+            process.parentNode.removeChild(DoneBox)
+          }, 1)
+          
+
+    } else {
+      running.progress++
+    }
+    clock++
+    WaitingProcesses.length > 0 ? '' : clearInterval(ProcessScan)
+  }, 1000)
 
 }
 
