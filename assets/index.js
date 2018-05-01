c69f682a3cad91c1105dddea45d8738a69773cc0
Mohammed <mido5219@gmail.com>
2018-05-02 01:26:06 +0200

    Adding Animation to Progress

--- assets/index.js
+++ assets/index.js
@@ -8,7 +8,6 @@ let burstTimeField = document.querySelector('#burst_time'),
   addbtn = document.querySelector('#addbtn')
 burstTimeField.addEventListener('input', (e) => {
   const message = document.querySelector('#burst_time_message')
-
   if (isNaN(e.srcElement.value)) {
     message.textContent = "Not a Number"
     addbtn.disabled = true
@@ -24,7 +23,6 @@ arrivedTimeField.addEventListener('input', (e) => {
   if (isNaN(e.srcElement.value)) {
     message.textContent = "Not a Number"
     addbtn.disabled = true
-
   } else {
     message.textContent = ''
     addbtn.disabled = false
@@ -47,36 +45,35 @@ AddProcess = () => {
     processes.push(current)
   }
 
-
   let row = document.createElement('tr'),
     processName = document.createElement('td'),
     processBurstTime = document.createElement('td'),
     processArrivedTime = document.createElement('td'),
     progressBar = document.createElement('div'),
     processProgress = document.createElement('td')
-
-  processName.textContent = `p${processNumber}`
-  processBurstTime.textContent = burstTime
-  processArrivedTime.textContent = arrivedTime
-  progressBar.style.width = "0%"
-  progressBar.setAttribute('id', `${current.name}`)
-  processProgress.classList.add('progress')
-  progressBar.classList.add('determinate')
-
-  processProgress.appendChild(progressBar)
-  row.appendChild(processName)
-  row.appendChild(processBurstTime)
-  row.appendChild(processArrivedTime)
-  row.appendChild(processProgress)
-  document.querySelector('tbody').appendChild(row)
-  row.style.opacity = `0`
-  let x = 0
-  let scaler = setInterval(() => {
-    row.style.opacity = `${x+=0.004}`
-    x >= 1 ? clearInterval(scaler) : ""
-  }, 1)
+    processName.textContent = `p${processNumber}`
+    processBurstTime.textContent = burstTime
+    processArrivedTime.textContent = arrivedTime
+    progressBar.style.width = "0%"
+    progressBar.setAttribute('id', `${current.name}`)
+    processProgress.classList.add('progress')
+    progressBar.classList.add('dynamicProgress')
+    progressBar.classList.add('determinate')
+    processProgress.appendChild(progressBar)
+    row.appendChild(processName)
+    row.appendChild(processBurstTime)
+    row.appendChild(processArrivedTime)
+    row.appendChild(processProgress)
+    document.querySelector('tbody').appendChild(row)
+    row.style.opacity = `0`
+    let x = 0
+    let scaler = setInterval(() => {
+      row.style.opacity = `${x+=0.004}`
+      x >= 1 ? clearInterval(scaler) : ""
+    }, 1)
 
 }
+
 ClearProcess = () => {
   processes = [];
   var myNode = document.querySelector('tbody');
@@ -85,13 +82,9 @@ ClearProcess = () => {
   }
 }
 
-
-
 StartSimulation = () => {
-  console.log('clicked')
   startbtn = document.querySelector('#startbtn').disabled = true
   addbtn = document.querySelector('#addbtn').disabled = true
-
   let processCounter = 0
   let dp = processes[0],
     running = processes[0]
@@ -105,15 +98,12 @@ StartSimulation = () => {
     processes.splice(processes.findIndex(p => p.name == running.name), 1)
   }
   let ProcessScan = setInterval(() => {
-
     processes.forEach((process) => {
       if (process.at == clock) {
         WaitingProcesses.push(process)
         let processBox = document.createElement('div')
-
         processBox.classList.add('col')
         let unit = (100 - (processes.length)) / totalbt
-        console.log(unit)
         let width = process.bt * unit
         processBox.setAttribute("style", "width:" + width + "%")
         processBox.id = process.name
@@ -123,7 +113,7 @@ StartSimulation = () => {
         processBox.style.background = RandomColor()
         processBox.style.transform = `translateX(100%)`
         processBox.style.opacity = `0`
-
+        processBox.style.textAlign = 'center'
         let processNameBox = document.createElement('h5')
         processNameBox.textContent = process.name
         processBox.appendChild(processNameBox)
@@ -140,11 +130,11 @@ StartSimulation = () => {
       }
     })
 
-
     if (running.progress == running.bt) {
       running.done = true
       processCounter++
       dp = WaitingProcesses.shift()
+      console.log
       WaitingProcesses.sort((p1, p2) => p1.bt - p2.bt)
       running = WaitingProcesses[0]
 
@@ -156,26 +146,32 @@ StartSimulation = () => {
         }
       })
       let x = 0
-          let y = 1
-          let scaler = setInterval(() => {
-            DoneBox.style.transform = `translateY(${++x}%)`
-            if (y > 0) {
-              DoneBox.style.opacity = `${y-=0.004}`
-            }
-            x == 200 && y==0 ? clearInterval(scaler) : ""
-            process.parentNode.removeChild(DoneBox)
-          }, 1)
-          
+      let y = 1
+      let scaler = setInterval(() => {
+        DoneBox.style.transform = `translateY(${++x}%)`
+        if (y > 0) {
+          DoneBox.style.opacity = `${y-=0.004}`
+        }
+        if(x == 200 && y == 0){
+          clearInterval(scaler)
+          DoneBox.parentNode.removeChild(DoneBox)
+        } 
+      }, 1)
 
     } else {
       running.progress++
+      let bars = document.querySelectorAll('.dynamicProgress')
+      bars.forEach((bar)=>{
+        if(bar.id == running.name){
+          bar.style.width = `${(running.progress/running.bt)*100}%`
+        }
+      })
     }
     clock++
     WaitingProcesses.length > 0 ? '' : clearInterval(ProcessScan)
   }, 1000)
 
 }
-
 
 RandomColor = () => {
   let color = ''
@@ -185,24 +181,3 @@ RandomColor = () => {
   }
   return `#${color}`
 }
-
-
-
-
-
-
-/*
- let processBox = document.createElement('div')
-    processBox.style.backgroundColor = RandomColor()
-    processBox.classList.add('col')
-    processBox.classList.add('s1')
-    processBox.classList.add('z-depth-3')
-    processBox.style.marginRight = "5px"
-    let processNameBox = document.createElement('h5')
-    let processWaitTime = document.createElement('h6')
-    processNameBox.textContent = currentprocess.name
-    console.log(currentprocess)
-    processBox.appendChild(processNameBox)
-    processBox.appendChild(processWaitTime)
-    timeline.appendChild(processBox)
-    */
